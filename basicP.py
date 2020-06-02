bc4b85f662fa18eca8163f9d8786d444177d3d65
Derek Sheppard <Dssheppard8@gmail.com>
2020-06-02 17:16:48 -0400

    python practice repository

---
+++ basicP.py
@@ -0,0 +1,20 @@
+#basic python practice
+#no real direction here merely testing basic python functionality
+
+x, y = 5,7
+z = "Derek "
+w = 'wow python is pretty neat'
+t = z + w
+
+
+print(z)
+if x < y:
+    print("yay maths")
+    print("wow " + z)
+print(t)
+print(x + y)
+
+def newfunc():
+    print(x - y)
+
+newfunc()
